6c0fcd3a88f10b3d539d24efc582f452674b88ba
Destiny911gtr <dhanush911gtr@gmail.com>
2020-04-21 21:32:12 +0530

    Add a link to my other project - https://github.com/Destiny911gtr/covid19-app

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import logging, os
 START_TEXT = 'COVID-19 info bot. Type /help to see what i can do'
 ABOUT_TEXT = 'Bot made by [Dhanush Krishnan](https://t.me/Ded_Boi) and [Marvin Clement](https://t.me/Credance). Forked from [PokeBot](https://t.me/Veg_Pokedex_Bot) ([Source](https://github.com/kshatriya-abhay/pokebot))'
 HELP_TEXT = "<b>List of available commands:</b> \n\n/help - Get this list \n/covid <i>state-name</i> - <i>Info on covid infection for specified state (India)</i> \n\n<b>Example - /covid Kerala</b> \n\n/dist <i>state-name</i> - <i>Info on covid infection for each district in specified state (India)</i> \n\n<b>Example - /dist Kerala</b>"
+APP_TEXT = "*COVID19 Info app.*\n_Made with flutter._\n\n[Google Drive](https://bit.ly/2XXGklQ)"
 
 def start(update, context):
     update.message.reply_text(text = START_TEXT)
@@ -49,6 +50,9 @@ def get_dist(update, context):
     else:
         update.message.reply_text("Usage: /dist state_name")
 
+def send_app(update, context):
+    update.message.reply_text(APP_TEXT, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
 if __name__ == "__main__":
     TOKEN = os.getenv('TOKEN')
 
@@ -66,6 +70,7 @@ if __name__ == "__main__":
     updater.dispatcher.add_handler(CommandHandler('help', get_help))
     updater.dispatcher.add_handler(CommandHandler('covid', get_info))
     updater.dispatcher.add_handler(CommandHandler('dist', get_dist))
+    updater.dispatcher.add_handler(CommandHandler('sendapp', send_app))
 
     # Start the webhook
     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)

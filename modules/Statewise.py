0669c1709f39c9c8c54abb97a843093493b4de7e
Dhanush Krishnan <dhanush911gtr@gmail.com>
2020-08-26 00:31:39 +0530

    New API URLs and bot updates

--- modules/Statewise.py
+++ modules/Statewise.py
@@ -1,6 +1,6 @@
 import pytz, requests, time, timeago, datetime, json
 
-url = "https://api.covid19india.org/data.json"
+url = "https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise"
 now = datetime.datetime.now()
 
 def getCovid(state="Kerala"):
@@ -9,19 +9,22 @@ def getCovid(state="Kerala"):
     json_url = requests.get(url)
     data = json.loads(json_url.text)
     states = data["statewise"]
-    for j in states:
-        if j["state"] == state:
-             k = j
-             break
+    if state == "Total":
+        states = data["total"]
     else:
-        failed = "Wrong input. Eg - <code>/covid Kerala</code> \n\nVisit https://www.covid19india.org/ for more info."
-        return failed
-    reft = str(k['lastupdatedtime'])
+        for j in states:
+            if j["state"] == state:
+                k = j
+                break
+        else:
+            failed = "Wrong input. Eg - <code>/covid Kerala</code> \n\nVisit https://www.covid19india.org/ for more info."
+            return failed
+    reft = str(k['lastRefreshed'])
     date = reft.split(" ", 1)[0]
     day = date.split("/", 2)[0]
     month = date.split("/", 2)[1]
     year = date.split("/", 2)[2]
     lref = year + "-" + month + "-" + day + " " + reft.split(" ", 1)[1]
     refresht = timeago.format(lref, curr_time_split)
-    status = str("<b>State: <u>{} - {}</u></b> \n<code>😷  Confirmed  |  {} \n🤗  Recovered  |  {} \n💀  Deaths     |  {} \n🤧  Total      |  {}</code> \n\n<b>New Cases</b> \n<code>😷  Confirmed  |  {} \n💀  Deaths     |  {} \n🤗  Recovered  |  {}</code> \n\n<i>Refreshed: {}</i>".format(k['state'], k['statecode'], k['active'], k['recovered'], k['deaths'], k['confirmed'], k['deltaconfirmed'], k['deltadeaths'], k['deltarecovered'], refresht))
+    status = str("<b>State: <u>{} - {}</u></b> \n<code>😷  Confirmed  |  {} \n🤗  Recovered  |  {} \n💀  Deaths     |  {} \n🤧  Total      |  {}</code> \n\n<i>Refreshed: {}</i>".format(k['state'], k['statecode'], k['active'], k['recovered'], k['deaths'], k['confirmed'], refresht))
     return status
